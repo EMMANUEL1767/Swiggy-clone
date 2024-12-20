f9b602bb31f3827dfb0082fce1ecfefc70f62921
Emmanuel Biju <emmanuelbiju4@gmail.com>
2024-12-20 23:02:19 +0530

    Initial Commit

---
+++ Swiggy/SwiggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiggyApp.swift
+//  Swiggy
+//
+//  Created by Emmanuel Biju on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
